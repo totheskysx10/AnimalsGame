17e7b066d14f6110968c67e88aa6b87964d7137c
Totheskysx10 <crazycat87654@gmail.com>
2025-03-04 19:40:02 +0500

    Initial commit

---
+++ src/main/java/com/good/animalsgame/AnimalsGameApplication.java
@@ -0,0 +1,13 @@
+package com.good.animalsgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalsGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalsGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/good/animalsgame/AnimalsGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.good.animalsgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalsGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
